b3cac3f811905376df83b5ececce059ea0594936
jjjjjaejin <double003388@gmail.com>
2024-03-16 00:52:32 +0900

    AI chap3 ex1

---
+++ my_code/ex1_chap3.py
@@ -0,0 +1,80 @@
+# 2024/03/15
+
+
+# (1)
+i = 100>200
+print(i)
+
+
+# (2)
+i = 100>=200
+print(i)
+
+
+# (3)
+i = 100<200
+print(i)
+
+
+# (4)
+i = 100<=200
+print(i)
+
+
+# (5)
+i = 100==200
+print(i)
+
+
+# (6)
+i = 100!=200
+print(i)
+
+
+# (7)
+i = 200==200
+print(i)
+
+
+# (8)
+i = True or True
+print(i)
+
+
+# (9)
+i = True or False
+print(i)
+
+
+# (10)
+i = True or False
+print(i)
+
+
+# (11)
+i = True and False
+print(i)
+
+
+# (12)
+i = True and True
+print(i)
+
+
+# (13)
+i = True or True and False
+print(i)
+
+# (14)
+i = True and True or False
+print(i)
+
+
+# (15)
+i = 'Moring' < 'moring'
+print(i)
+
+
+# (16)
+i = 'A'<'B'
+print(i)
