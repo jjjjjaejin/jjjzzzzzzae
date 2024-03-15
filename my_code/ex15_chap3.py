3f13c1ff4816597ca6a2c372389ba9e188becccb
jjjjjaejin <double003388@gmail.com>
2024-03-16 01:04:26 +0900

    AI chap3 ex15

---
+++ my_code/ex15_chap3.py
@@ -0,0 +1,15 @@
+# 2024/03/16
+# 반복문을 이용해 구구단 2절 출력
+
+
+# for문
+for i in range(1,10):
+    print('2 *',i, '=', 2*i)
+
+print()
+
+# while문
+i = 1
+while i<10:
+    print('2 *', i, '=', 2*i)
+    i += 1
