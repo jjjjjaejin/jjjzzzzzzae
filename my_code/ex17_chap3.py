06d13f721f196a0cb12b11b7c24d55650c405df3
jjjjjaejin <double003388@gmail.com>
2024-03-16 15:57:07 +0900

    AI chap3 ex17

---
+++ my_code/ex17_chap3.py
@@ -0,0 +1,29 @@
+# 2024/03/16
+
+
+
+# (1)
+for i in range(3):
+    print('Python ')
+    print('is ')
+    print('FUN! ')
+
+
+print()
+
+
+# (2)
+for i in range(3):
+    print('Python ')
+    print('is ')
+print('FUN! ')
+
+
+print()
+
+
+# (3)
+for i in range(3):
+    print('Python ')
+print('is ')
+print('FUN! ')
